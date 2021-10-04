dd1ff86259bb4509bab2152f8551e4704a2f20b9
rajkumbhar <rrajkumbhar@gmail.com>
2021-10-05 01:56:49 +0530

    Index page - Worked on top bar

---
+++ dream-shelter-forum/utils/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  appBar: {
+    backgroundColor: 'darkgreen',
+    boxShadow: 'none',
+  },
+});
+
+export default useStyles;
